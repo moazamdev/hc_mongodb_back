91b828b80a97a719d0764b19d02cec7c2dfb1ad0
Moazam Ali <mozalikorai@gmail.com>
2023-05-24 11:24:09 +0500

    Updated backend code

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
 const mysql = require("mysql");
 const cors = require("cors");
-require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"Content-Type, Authorization"
-	);
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.setHeader("Access-Control-Allow-Origin", "*");
+// 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+// 	res.setHeader(
+// 		"Access-Control-Allow-Headers",
+// 		"Content-Type, Authorization"
+// 	);
+// 	next();
+// });
 
 const db = mysql.createConnection({
 	user: process.env.DB_USER,
@@ -24,6 +24,12 @@ const db = mysql.createConnection({
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 });
+// const db = mysql.createConnection({
+// 	user: "root",
+// 	host: "localhost",
+// 	password: "moz@119!",
+// 	database: "hemocelldb",
+// });
 
 // db.connect(function (err) {
 // 	if (err) {
